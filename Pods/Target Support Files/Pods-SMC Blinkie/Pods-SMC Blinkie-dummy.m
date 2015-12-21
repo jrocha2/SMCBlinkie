7a9cc027bef65e2099b4b5f631b8fad9a9efaee8
John Rocha <john.rocha5@gmail.com>
2015-12-20 18:39:18 -0600

    Initialized Firebase with Swift 2.0

---
+++ Pods/Target Support Files/Pods-SMC Blinkie/Pods-SMC Blinkie-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SMC_Blinkie : NSObject
+@end
+@implementation PodsDummy_Pods_SMC_Blinkie
+@end
---
+++ Pods/Target Support Files/Pods-SMC Blinkie/Pods-SMC Blinkie-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SMC_BlinkieVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SMC_BlinkieVersionString[];
+

92a3b3bbff9c58913a933abcbb7043dd43147879
John Rocha <john.rocha5@gmail.com>
2015-12-30 00:56:46 -0600

    Now displays user's current location

--- SMC Blinkie/MapViewController.swift
+++ SMC Blinkie/MapViewController.swift
@@ -13,18 +13,30 @@ class MapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         mapView.delegate = self
     }
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
+        checkLocationAuthorizationStatus()
         
         // Center map whenever the map appears
         let centerLocation = CLLocation(latitude: 41.703002, longitude: -86.249173)
         centerMapOnLocation(centerLocation, width: 3700, height: 1100)
+    }
+    
+    // checks that user has authorized location tracking
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+            checkLocationAuthorizationStatus()
+        }
     }
     
     // Centers the map on a given coordinate with provided map width and height
